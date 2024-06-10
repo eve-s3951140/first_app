90cf89fe3f91aab87f3ded4c2637f76c8d1d9ba1
evelynlie <evelynlie15@gmail.com>
2024-06-11 00:33:08 +1000

    Split Center Text into new file

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -17,12 +18,7 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomCenter,
         ),
       ),
-      child: const Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(fontSize: 30, color: Colors.white),
-        ),
-      ),
+      child: const StyledText(),
     );
   }
 }
