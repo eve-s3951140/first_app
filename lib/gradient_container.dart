ceba08ce3d81451bd611133a34c56779b1952d95
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-06-10 23:35:21 +1000

    Split code

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(152, 35, 35, 1),
+            Color.fromRGBO(29, 95, 119, 1),
+            Color.fromRGBO(7, 22, 89, 1),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(fontSize: 30, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp( // Show user interface on the screen
+    const MaterialApp(
+      // Show user interface on the screen
       home: Scaffold(
         backgroundColor: Color.fromRGBO(29, 95, 119, 0),
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(152, 35, 35, 1),
-            Color.fromRGBO(29, 95, 119, 1),
-            Color.fromRGBO(7, 22, 89, 1),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: const Center(
-        child: Text("Hello World", 
-          style: TextStyle(fontSize: 30, color: Colors.white),
-        ),
-      ),
-    );
-  }
 }
